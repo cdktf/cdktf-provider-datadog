bb821c687f91223c4564f59f2994bfd8c44975b1
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2021-12-09 14:35:49 +0000

    chore(deps): upgrade provider project

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { CdktfProviderProject } = require('@cdktf/provider-project');
 const project = new CdktfProviderProject({
   terraformProvider: "DataDog/datadog@~> 3.0",
-  cdktfVersion: '^0.8.1',
+  cdktfVersion: '^0.8',
   constructsVersion: "^10.0.0",
   minNodeVersion: "12.19.0",
   jsiiVersion: "^1.46.0",
