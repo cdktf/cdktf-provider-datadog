5e78b70fad9314db398af9d904cb5a5bc3956877
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2022-01-19 04:09:02 +0000

    chore: upgrade provider

--- src/security-monitoring-rule.ts
+++ src/security-monitoring-rule.ts
@@ -402,13 +402,13 @@ export class SecurityMonitoringRuleOptionsOutputReference extends cdktf.ComplexO
 }
 export interface SecurityMonitoringRuleQueryAgentRule {
   /**
-  * The Agent rule ID. Must be unique within the rule.
+  * **Deprecated**. It won't be applied anymore.
   * 
   * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/datadog/r/security_monitoring_rule#agent_rule_id SecurityMonitoringRule#agent_rule_id}
   */
   readonly agentRuleId: string;
   /**
-  * A Runtime Security expression determines what activity should be collected by the Datadog Agent. These logical expressions can use predefined operators and attributes. Tags cannot be used in Runtime Security expressions. Instead, allow or deny based on tags under the advanced option.
+  * **Deprecated**. It won't be applied anymore.
   * 
   * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/datadog/r/security_monitoring_rule#expression SecurityMonitoringRule#expression}
   */
