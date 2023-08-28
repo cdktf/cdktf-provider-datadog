ce798a241183327c59770d95e3797c07f6f39ea8
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-28 10:02:10 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@9fe1871

--- src/data-datadog-role/index.ts
+++ src/data-datadog-role/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/datadog/datadog/3.29.0/docs/data-sources/role
 // generated from terraform resource schema
 
