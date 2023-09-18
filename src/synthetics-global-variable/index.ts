701157a57d094254a8f6bcb6ccb8adabba3e4565
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-18 15:02:57 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@d2a7f81

--- src/synthetics-global-variable/index.ts
+++ src/synthetics-global-variable/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/datadog/datadog/3.30.0/docs/resources/synthetics_global_variable
 // generated from terraform resource schema
 
