0e7b3098235500ab2e2915eb25a4a2a2eab18517
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-05 10:27:44 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/logs-custom-pipeline/index.ts
+++ src/logs-custom-pipeline/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.35.0/docs/resources/logs_custom_pipeline
 // generated from terraform resource schema
 
