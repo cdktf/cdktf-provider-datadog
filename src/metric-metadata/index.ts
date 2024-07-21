db6d9ae2084f175dd592b0e24f4f06029476be48
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-21 05:56:26 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/metric-metadata/index.ts
+++ src/metric-metadata/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.41.0/docs/resources/metric_metadata
 // generated from terraform resource schema
 
