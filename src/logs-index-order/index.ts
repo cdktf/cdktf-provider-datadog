0966c399701e0ad3582b10cbcfda3191dd9d3203
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-08 05:57:14 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/logs-index-order/index.ts
+++ src/logs-index-order/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.44.0/docs/resources/logs_index_order
 // generated from terraform resource schema
 
