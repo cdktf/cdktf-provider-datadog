ddba4359d330dd42a744e7def56f9412d0096237
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-27 05:58:16 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/downtime-schedule/index.ts
+++ src/downtime-schedule/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.46.0/docs/resources/downtime_schedule
 // generated from terraform resource schema
 
