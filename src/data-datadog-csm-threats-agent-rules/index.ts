8057dfda226b0c7fefcb3e49d710271f83476eeb
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-17 05:59:31 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-datadog-csm-threats-agent-rules/index.ts
+++ src/data-datadog-csm-threats-agent-rules/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.48.1/docs/data-sources/csm_threats_agent_rules
 // generated from terraform resource schema
 
