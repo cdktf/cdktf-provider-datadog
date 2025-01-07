7f3ebe6a9c06c3effc45efe5356ec2ff4f0b71d6
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-07 10:58:37 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/synthetics-concurrency-cap/index.ts
+++ src/synthetics-concurrency-cap/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.50.0/docs/resources/synthetics_concurrency_cap
 // generated from terraform resource schema
 
