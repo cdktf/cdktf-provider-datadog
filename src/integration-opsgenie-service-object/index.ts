af83acf5520262b90a466f408ff7d3f45d8ccc93
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-16 12:17:43 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@63a530e

--- src/integration-opsgenie-service-object/index.ts
+++ src/integration-opsgenie-service-object/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/datadog/datadog/3.52.0/docs/resources/integration_opsgenie_service_object
 // generated from terraform resource schema
 
