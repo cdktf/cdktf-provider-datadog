a0d330f2f93275b46a6e70062fec5e4a99467396
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-23 05:54:29 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c0813b

--- src/data-datadog-integration-aws-available-namespaces/index.ts
+++ src/data-datadog-integration-aws-available-namespaces/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/datadog/datadog/3.58.0/docs/data-sources/integration_aws_available_namespaces
 // generated from terraform resource schema
 
