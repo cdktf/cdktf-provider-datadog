e44d6db1932afe2b6a6e07d7033072b80df21e6b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:46:28 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/compliance-custom-framework/index.ts
+++ src/compliance-custom-framework/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.65.0/docs/resources/compliance_custom_framework
 // generated from terraform resource schema
 
