db9cf12e6ed3d24bef2c22de3faee198435f1d83
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:39:40 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/apm-retention-filter/index.ts
+++ src/apm-retention-filter/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/datadog/datadog/3.65.0/docs/resources/apm_retention_filter
 // generated from terraform resource schema
 
